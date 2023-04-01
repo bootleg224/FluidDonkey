553e1c40d602b30c9ba73f86af0f9396dd7b4472
bootleg224 <kjohar@getopenwater.com>
2023-04-01 13:59:33 -0400

    Complete HTML Template Example and Mock Data

--- SampleApp/Controllers/HomeController.cs
+++ SampleApp/Controllers/HomeController.cs
@@ -5,111 +5,29 @@ using Fluid;
 using ClosedXML.Excel;
 
 
+
 namespace SampleApp.Controllers
 {
 	public class HomeController : Controller
 	{
 		private readonly ILogger<HomeController> _logger;
+		private readonly IWebHostEnvironment _host;
 
-		public HomeController(ILogger<HomeController> logger)
+		public HomeController(ILogger<HomeController> logger, IWebHostEnvironment host)
 		{
 			_logger = logger;
+			_host = host;
 		}
 
-		public IActionResult Index()
+	
+		public IActionResult Index(string templateName)
 		{
-			return View();
-		}
+			var workOrder = WorkOrder.GetSampleWorkOrder();
 
-		public IActionResult Privacy()
-		{
-			return View();
-		}
-
-		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-		public IActionResult Error()
-		{
-			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-		}
-
-		public IActionResult GenerateFluid()
-		{
-			var Company = new Company
-			{
-				Name = "Acme Co.",
-				Address = "123 Main St",
-				City = "Anytown",
-				State = "CA",
-				ZipCode = "12345",
-				LogoUrl = "http://example.com/logo.png"
-			};
-
-			var Customer = new Customer
-			{
-				Name = "John Doe",
-				Address = "456 Broadway St",
-				City = "Anytown",
-				State = "CA",
-				ZipCode = "12345",
-				Email = "john.doe@example.com"
-			};
-
-			var items = new List<WorkOrderItem>
-{
-	new WorkOrderItem
-	{
-		ProductName = "Product 1",
-		UnitPrice = 10.00m,
-		Quantity = 2,
-		RecurringOrOneTime = "One-Time"
-	},
-	new WorkOrderItem
-	{
-		ProductName = "Product 2",
-		UnitPrice = 5.00m,
-		Quantity = 3,
-		RecurringOrOneTime = "Recurring"
-	},
-	new WorkOrderItem
-	{
-		ProductName = "Product 3",
-		UnitPrice = 7.50m,
-		Quantity = 1,
-		RecurringOrOneTime = "One-Time"
-	},
-	new WorkOrderItem
-	{
-		ProductName = "Product 4",
-		UnitPrice = 20.00m,
-		Quantity = 1,
-		RecurringOrOneTime = "One-Time"
-	},
-	new WorkOrderItem
-	{
-		ProductName = "Product 5",
-		UnitPrice = 15.00m,
-		Quantity = 2,
-		RecurringOrOneTime = "Recurring"
-	}
-};
-
-			var productData = new List<Product>
-{
-	new Product { ProductName = "Product 1", Description = "This is the first product" },
-	new Product { ProductName = "Product 2", Description = "This is the second product" },
-	new Product { ProductName = "Product 3", Description = "This is the third product" },
-	new Product { ProductName = "Product 4", Description = "This is the fourth product" },
-	new Product { ProductName = "Product 5", Description = "This is the fifth product" }
-};
-
-			var workOrder = new WorkOrder
-			{
-				Company = Company,
-				Customer = Customer,
-				Items = items,
-				TermsAndConditions = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat.",
-				Products = productData
-			};
+			if (string.IsNullOrEmpty(templateName))
+				templateName = "WorkOrder";
+						
+			var FluidTemplate = System.IO.File.ReadAllText(Path.Combine(_host.ContentRootPath, @$"Templates\{templateName}.html"));
 
 			var parser = new FluidParser();
 			
@@ -137,82 +55,9 @@ namespace SampleApp.Controllers
 
 		public IActionResult GenerateExcel()
 		{
-			var Company = new Company
-			{
-				Name = "Acme Co.",
-				Address = "123 Main St",
-				City = "Anytown",
-				State = "CA",
-				ZipCode = "12345",
-				LogoUrl = "https://res.cloudinary.com/crunchbase-production/image/upload/c_lpad,h_170,w_170,f_auto,b_white,q_auto:eco,dpr_1/v1398384448/onoi8gdjjxsin78cxd82.png"
-			};
 
-			var Customer = new Customer
-			{
-				Name = "John Doe",
-				Address = "456 Broadway St",
-				City = "Anytown",
-				State = "CA",
-				ZipCode = "12345",
-				Email = "john.doe@example.com"
-			};
 
-			var items = new List<WorkOrderItem>
-{
-	new WorkOrderItem
-	{
-		ProductName = "Product 1",
-		UnitPrice = 10.00m,
-		Quantity = 2,
-		RecurringOrOneTime = "One-Time"
-	},
-	new WorkOrderItem
-	{
-		ProductName = "Product 2",
-		UnitPrice = 5.00m,
-		Quantity = 3,
-		RecurringOrOneTime = "Recurring"
-	},
-	new WorkOrderItem
-	{
-		ProductName = "Product 3",
-		UnitPrice = 7.50m,
-		Quantity = 1,
-		RecurringOrOneTime = "One-Time"
-	},
-	new WorkOrderItem
-	{
-		ProductName = "Product 4",
-		UnitPrice = 20.00m,
-		Quantity = 1,
-		RecurringOrOneTime = "One-Time"
-	},
-	new WorkOrderItem
-	{
-		ProductName = "Product 5",
-		UnitPrice = 15.00m,
-		Quantity = 2,
-		RecurringOrOneTime = "Recurring"
-	}
-};
-
-			var productData = new List<Product>
-{
-	new Product { ProductName = "Product 1", Description = "This is the first product" },
-	new Product { ProductName = "Product 2", Description = "This is the second product" },
-	new Product { ProductName = "Product 3", Description = "This is the third product" },
-	new Product { ProductName = "Product 4", Description = "This is the fourth product" },
-	new Product { ProductName = "Product 5", Description = "This is the fifth product" }
-};
-
-			var workOrder = new WorkOrder
-			{
-				Company = Company,
-				Customer = Customer,
-				Items = items,
-				TermsAndConditions = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat.",
-				Products = productData
-			};
+			var workOrder = WorkOrder.GetSampleWorkOrder();
 
 
 			var wb = new XLWorkbook();
@@ -223,11 +68,7 @@ namespace SampleApp.Controllers
 			//			 .MoveTo(ws.Cell(1, 1))
 			//			 .WithSize(100, 100);
 
-			// Add company name and address to worksheet
-			ws.Cell(1, 2).Value = workOrder.Company.Name;
-			ws.Cell(2, 2).Value = workOrder.Company.Address;
-			ws.Cell(3, 2).Value = workOrder.Company.City + ", " + workOrder.Company.State + " " + workOrder.Company.ZipCode;
-
+			
 			// Add customer information to worksheet
 			ws.Cell(5, 1).Value = "Customer Information";
 			ws.Cell(6, 1).Value = "Name:";
@@ -251,7 +92,7 @@ namespace SampleApp.Controllers
 				ws.Cell(rowIndex, 1).Value = item.ProductName;
 				ws.Cell(rowIndex, 2).Value = item.UnitPrice;
 				ws.Cell(rowIndex, 3).Value = item.Quantity;
-				ws.Cell(rowIndex, 4).Value = item.RecurringOrOneTime;
+				ws.Cell(rowIndex, 4).Value = item.RecurringStyle == ProductRevenueRecurringStyle.Recurring ? "Recurring" : "One-Time";
 				ws.Cell(rowIndex, 5).FormulaA1 = string.Format("B{0}*C{0}", rowIndex);
 				rowIndex++;
 			}
@@ -273,211 +114,6 @@ namespace SampleApp.Controllers
 			wb.SaveAs(ms);
 
 			return File(ms.ToArray(), "application/download", "workbook.xlsx");
-		}
-
-
-
-		public const string FluidTemplate = @"<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset=""utf-8"">
-    <title>Work Order</title>
-<style>
-  body {
-    font-family: Arial, sans-serif;
-  }
-
-  .header {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    margin-bottom: 20px;
-  }
-
-  .logo img {
-    max-height: 100px;
-  }
-
-  .Company-info p {
-    margin: 0;
-  }
-
-  .Customer-info {
-    margin-bottom: 20px;
-  }
-
-  .items {
-    margin-bottom: 20px;
-  }
-
-  .items table {
-    width: 100%;
-    border-collapse: collapse;
-  }
-
-  .items table th,
-  .items table td {
-    border: 1px solid #ddd;
-    padding: 8px;
-  }
-
-  .items table th {
-    text-align: left;
-    background-color: #f2f2f2;
-  }
-
-  .subtotal {
-    margin-top: 20px;
-  }
-
-  .terms {
-    margin-bottom: 20px;
-  }
-
-  .signature {
-    margin-bottom: 20px;
-  }
-
-  .page-break {
-    page-break-after: always;
-  }
-
-  .products table {
-    width: 100%;
-    border-collapse: collapse;
-  }
-
-  .products table th,
-  .products table td {
-    border: 1px solid #ddd;
-    padding: 8px;
-  }
-
-  .products table th {
-    text-align: left;
-    background-color: #f2f2f2;
-  }
-</style>
-
-  </head>
-  <body>
-    <div class=""header"">
-      <div class=""logo"">
-        <img src=""{{ Company.LogoUrl }}"" alt=""{{ Company.Name }} logo"">
-      </div>
-      <div class=""Company-info"">
-        <p>{{ Company.Name }}</p>
-        <p>{{ Company.Address }}, {{ Company.City }}, {{ Company.State }} {{ Company.ZipCode }}</p>
-      </div>
-    </div>
-    <div class=""Customer-info"">
-      <h2>Customer Information</h2>
-      <p><strong>Name:</strong> {{ Customer.Name }}</p>
-      <p><strong>Address:</strong> {{ Customer.Address }}, {{ Customer.City }}, {{ Customer.State }} {{ Customer.ZipCode }}</p>
-      <p><strong>Email:</strong> {{ Customer.Email }}</p>
-    </div>
-    <div class=""items"">
-      <h2>Work Order Items</h2>
-      <table>
-        <thead>
-          <tr>
-            <th>Product Name</th>
-            <th>Unit Price</th>
-            <th>Quantity</th>
-            <th>Recurring/One-Time</th>
-            <th>Subtotal</th>
-          </tr>
-        </thead>
-        <tbody>
-          {% for item in Items %}
-          <tr>
-            <td>{{ item.ProductName }}</td>
-            <td>${{ item.UnitPrice }}</td>
-            <td>{{ item.Quantity }}</td>
-            <td>{{ item.RecurringOrOneTime }}</td>
-            <td>${ item.UnitPrice * item.Quantity }</td>
-          </tr>
-          {% endfor %}
-        </tbody>
-      </table>
-      <div class=""subtotal"">
-        <p>Subtotal: ${{ Items | map: ""UnitPrice"" | times: ""Quantity"" | sum }}</p>
-      </div>
-    </div>
-    <div class=""terms"">
-      <p><strong>Terms and Conditions:</strong> {{ order.TermsAndConditions }}</p>
-    </div>
-    <div class=""signature"">
-      <label for=""signature"">Signature:</label>
-      <input type=""text"" id=""signature"" name=""signature"">
-      <button>Submit</button>
-    </div>
-    <div class=""page-break""></div>
-    <div class=""products"">
-      <h2>Products</h2>
-      <table>
-        <thead>
-          <tr>
-            <th>Product Name</th>
-            <th>Description</th>
-          </tr>
-        </thead>
-        <tbody>
-          {% for product in Products %}
-          <tr>
-            <td>{{ product.ProductName }}</td>
-            <td>{{ product.Description }}</td>
-          </tr>
-          {% endfor %}
-        </tbody>
-      </table>
-    </div>
-  </body>
-</html>";
-	}
-
-	#region DataModel
-	public class Company
-	{
-		public string Name { get; set; }
-		public string Address { get; set; }
-		public string City { get; set; }
-		public string State { get; set; }
-		public string ZipCode { get; set; }
-		public string LogoUrl { get; set; }
-	}
-
-	public class Customer
-	{
-		public string Name { get; set; }
-		public string Address { get; set; }
-		public string City { get; set; }
-		public string State { get; set; }
-		public string ZipCode { get; set; }
-		public string Email { get; set; }
-	}
-
-	public class WorkOrderItem
-	{
-		public string ProductName { get; set; }
-		public decimal UnitPrice { get; set; }
-		public int Quantity { get; set; }
-		public string RecurringOrOneTime { get; set; }
-	}
-
-	public class WorkOrder
-	{
-		public Company Company { get; set; }
-		public Customer Customer { get; set; }
-		public List<WorkOrderItem> Items { get; set; }
-		public string TermsAndConditions { get; set; }
-	public List<Product> Products { get;  set; }
-}
-
-	public class Product
-	{
-		public string ProductName { get; set; }
-		public string Description { get; set; }
-	}
-	#endregion
+		}		
+	}	
 }
---
+++ SampleApp/Models/MockDatabase.cs
@@ -0,0 +1,243 @@
+﻿namespace SampleApp.Models
+{
+	public class Company
+	{
+		public string Name { get; set; }
+		public string Address { get; set; }
+		public string City { get; set; }
+		public string State { get; set; }
+		public string ZipCode { get; set; }
+		public string LogoUrl { get; set; }
+	}
+
+	public class Customer
+	{
+		public string Name { get; set; }
+		public string Address { get; set; }
+		public string City { get; set; }
+		public string State { get; set; }
+		public string ZipCode { get; set; }
+		public string Email { get; set; }
+	}
+
+	public class WorkOrderItem
+	{
+		public string ProductName { get; set; }
+
+		public ProductRevenueStyle RevenueStyle { get; set; }
+		public string RevenueStyleStr { get { return RevenueStyle.ToString(); } }
+
+		public ProductQuantityStyle QuantityStyle { get; set; }
+		public string QuantityStyleStr { get { return QuantityStyle.ToString(); } }
+
+		public ProductRevenueRecurringStyle RecurringStyle { get; set; }
+		public string RecurringStyleStr { get { return RecurringStyle.ToString(); } }
+
+		public decimal UnitPrice { get; set; }
+		public decimal Quantity { get; set; }
+
+		public decimal LineDiscount { get; set; }
+		public decimal ComputedLinePrice { get
+			{
+				return (UnitPrice * Quantity) - LineDiscount;
+			} 
+		}
+
+		public string SortKey { get; set; }
+		
+	}
+
+	public class WorkOrder
+	{
+		
+		public string WorkOrderId { get; set; }
+		public Customer Customer { get; set; }
+		public List<WorkOrderItem> Items { get; set; }
+		public string TermsAndConditions { get; set; }
+		public List<Product> Products { get; set; }
+		public DealInformation DealInfo { get; set; }
+		public DealSummary DealSummary { get; set; }
+
+		public static WorkOrder GetSampleWorkOrder()
+		{
+			var Customer = new Customer
+			{
+				Name = "Software Distributors",
+				Address = "456 Broadway St",
+				City = "Anytown",
+				State = "CA",
+				ZipCode = "12345",
+				Email = "john.doe@example.com"
+			};
+
+			var DealInfo = new DealInformation
+			{
+				Currency = "USD",
+				StartDate = new DateTime(2023, 4, 1),
+				EndDate = new DateTime(2023, 3, 31),
+				Term = "1 Year Agreement",
+				SalesPerson = "Brian Murphy",
+				UserCount = 23
+			};
+
+			var items = new List<WorkOrderItem>();
+
+			items.Add(new WorkOrderItem
+			{
+				ProductName = "iMIS EMS Enterprise",
+				Quantity = 23M,
+				 QuantityStyle = ProductQuantityStyle.ByUserCount,
+				 RevenueStyle = ProductRevenueStyle.QuantityBased,
+				 RecurringStyle = ProductRevenueRecurringStyle.Recurring,
+				 SortKey = "00_IMIS__01",
+				 LineDiscount = 0M,
+				 UnitPrice = 230.0M			
+			});
+
+			items.Add(new WorkOrderItem
+			{
+				ProductName = "iMIS Campaign Management",
+				Quantity = 23M,
+				QuantityStyle = ProductQuantityStyle.ByUserCount,
+				RevenueStyle = ProductRevenueStyle.QuantityBased,
+				RecurringStyle = ProductRevenueRecurringStyle.Recurring,
+				SortKey = "00_IMIS__02",
+				LineDiscount = -150M,
+				UnitPrice = 63.0M
+			});
+
+
+
+			items.Add(new WorkOrderItem
+			{
+				ProductName = "OpenWater Software Platform Fee",
+				Quantity = 1M,
+				QuantityStyle = ProductQuantityStyle.Normal,
+				RevenueStyle = ProductRevenueStyle.QuantityBased,
+				RecurringStyle = ProductRevenueRecurringStyle.Recurring,
+				SortKey = "10_OPENWATER_001",
+				LineDiscount = 0M,
+				UnitPrice = 4950
+			});
+
+			items.Add(new WorkOrderItem
+			{
+				ProductName = "OpenWater Software Per Program Fee",
+				Quantity = 2M,
+				QuantityStyle = ProductQuantityStyle.Normal,
+				RevenueStyle = ProductRevenueStyle.QuantityBased,
+				RecurringStyle = ProductRevenueRecurringStyle.Recurring,
+				SortKey = "10_OPENWATER_002",
+				LineDiscount = -650M,
+				UnitPrice = 1250
+			});
+
+			items.Add(new WorkOrderItem
+			{
+				ProductName = "Integrations Hub",
+				Quantity = 2M,
+				QuantityStyle = ProductQuantityStyle.Normal,
+				RevenueStyle = ProductRevenueStyle.Fixed,
+				RecurringStyle = ProductRevenueRecurringStyle.Recurring,
+				SortKey = "10_OPENWATER_003",
+				LineDiscount = 0M,
+				UnitPrice = 1250
+			});
+
+			items.Add(new WorkOrderItem
+			{
+				ProductName = "Consulting Project",
+				Quantity = 1M,
+				QuantityStyle = ProductQuantityStyle.Normal,
+				RevenueStyle = ProductRevenueStyle.Fixed,
+				RecurringStyle = ProductRevenueRecurringStyle.OneTime,
+				SortKey = "10_OPENWATER_004",
+				LineDiscount = 0M,
+				UnitPrice = 25000
+			});
+
+			//summarize deal
+			var dealSummary = new DealSummary();
+			dealSummary.TotalOneTime = items.Where(c=>c.RecurringStyle == ProductRevenueRecurringStyle.OneTime).Sum(c => c.ComputedLinePrice);
+			dealSummary.TotalRecurring= items.Where(c => c.RecurringStyle == ProductRevenueRecurringStyle.Recurring).Sum(c => c.ComputedLinePrice);
+
+			if (DealInfo.UserCount > 0)
+			{
+				var totalUserSpecificCharges = items.Where(c => c.QuantityStyle == ProductQuantityStyle.ByUserCount).Sum(c => c.ComputedLinePrice);
+				dealSummary.TotalRecurringForUserSpecificItems = totalUserSpecificCharges / DealInfo.UserCount;
+			}
+
+
+
+			var products = new List<Product>();
+			products.Add(new Product { ProductName = "iMIS EMS Enterprise", Description = "Description of Product" });
+			products.Add(new Product { ProductName = "iMIS Campaign Management", Description = "Description of Product" });
+			products.Add(new Product { ProductName = "OpenWater Software Platform Fee", Description = "Description of Product" });
+			products.Add(new Product { ProductName = "OpenWater Software Per Program Fee", Description = "Description of Product" });
+			products.Add(new Product { ProductName = "Integrations Hub", Description = "Description of Product" });
+			products.Add(new Product { ProductName = "Consulting Project", Description = "Description of Product" });
+
+
+
+
+			var workOrder = new WorkOrder
+			{				
+				DealInfo = DealInfo,
+				Customer = Customer,
+				Items = items.OrderBy(s=>s.SortKey).ToList(),
+				TermsAndConditions = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat.",
+				Products = products,
+				DealSummary = dealSummary
+			};
+
+			return workOrder;
+		}
+	}
+
+	public class DealSummary
+	{
+		public decimal TotalRecurring { get; set; }
+		public decimal TotalOneTime { get; set; }
+		public decimal ManualTotalDiscount { get; set; }
+
+		public decimal Total { get { return TotalOneTime + TotalRecurring - ManualTotalDiscount; } }
+
+		public decimal TotalRecurringForUserSpecificItems { get; set; }
+	}
+
+	public class Product
+	{
+		public string ProductName { get; set; }
+		public string Description { get; set; }
+	}
+
+	public class DealInformation
+	{
+		public DateTime StartDate { get; set; }
+		public string Term { get; set; }
+		public DateTime EndDate { get; set; }
+
+		public string Currency { get; set; }
+
+		public string SalesPerson { get; set; }
+	public int UserCount { get; set; }
+}
+
+	public enum ProductRevenueStyle
+	{
+		QuantityBased,
+		Fixed
+	}
+
+	public enum ProductRevenueRecurringStyle
+	{
+		OneTime,
+		Recurring
+	}
+
+	public enum ProductQuantityStyle
+	{
+		Normal,
+		ByUserCount
+	}
+}
